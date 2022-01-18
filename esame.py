799461e7b212df87c2320c863a5817d6c6f5a2d4
MartaLucas <94189892+15Max@users.noreply.github.com>
2022-01-18 15:22:41 +0000

    Cambiato ratio, non può essere negativo

--- esame.py
+++ esame.py
@@ -1,54 +1,51 @@
 class ExamException(Exception):
-    pass    
+   pass
 
-
-
-class MovingAverage():
-    def __init__(self, length):
-        self.length = length
+class Diff():
+    def __init__(self, ratio=1):
+        self.ratio = ratio 
+        #Controllo che ratio sia un numero
+        if not isinstance(ratio , int) and not isinstance(ratio , float):
+            raise ExamException('Il parametro ratio inserito non è un numero!')
         
-        # Controllo che la lunghezza sia un intero
-        if  not isinstance(self.length , int):
-            raise ExamException("L'input non è un intero!" )
-        # Controllo che la lunghezza sia strettamente positiva 
-        if self.length <= 0 :
-            raise ExamException('La lunghezza della finestra è negativa o nulla')
-        
+        #Controllo che ratio sia diverso da zero
+        if self.ratio <= 0:
+            raise ExamException('Ratio non deve essere minore o uguale a zero')
 
 
-    def compute(self , series ) :
-        #Controllo che la serie sia effettivamente una lista
-        if not isinstance(series, list):
-            raise ExamException("L'input non è una lista")
-
-        #Controllo che la serie non sia una lista vuota
-        elif series == [] :
-            raise ExamException('La lista contenente la serie è vuota')
-        
-        # Se la lista non è vuota, e solo allora , posso procedre con gli altri controlli
+    def compute(self, input_list) :
+        #Controllo che l'input sia effettivamente una lista
+        if not isinstance(input_list, list):
+            raise ExamException("L'input non è una lista!" )
+        #Se l'input è una lista controllo che questa non sia vuota
+        elif input_list == []:
+            raise ExamException('La lista degli input è vuota')
         else:
-            #Controllo che la lunghezza della finestra non ecceda quella della serie
-            if self.length > len(series):
-                raise ExamException('La lunghezza della finestra è più grande di quella della serie')
+            #Controllo che la lista abbia almeno due elementi
+            if len(input_list) < 2:
+                raise ExamException('La lista ha meno di due elementi')
+            # Contollo che la lista contenga numeri
+            for item in input_list:
+                if not isinstance(item,int) and not isinstance(item,float):
+                    raise ExamException('Uno degli elementi della lista non è un numero!')
 
-            # Controllo che gli elementi della serie siano numeri
-            for item in series:
-                if not isinstance(item, int) and not isinstance(item,float):
-                    raise ExamException('Uno degli elementi della lista non è un numero')
-        
-        
-        
-        
-        #Uso una lista vuota per salvare i risultati della media
+       
+
+
+        #creo una lista vuota in cui inserire i risultati
         results = []
-        # Faccio la media degli elementi richiesti e li salvo in results
-        for x in range(len(series)-self.length + 1):
-            element = sum(series[x : x+self.length])
-            element /= self.length
+
+        for index in range(len(input_list)-1):
+            #Calcolo la differenza degli elementi nella lista
+            element = input_list[index+1] - input_list[index]
+            #Se il paramtro ratio è stato inserito 'riscalo' l'elemento ottenuto
+            element /= self.ratio
+            #Inserisco l'elemento calcolato nella lista dei risultati
             results.append(element)
+
         return results
 
 
 
 
-
+            
